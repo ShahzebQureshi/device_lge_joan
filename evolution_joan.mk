52f0c488d2b8da1be52132749266e4a7ba996812
ShahzebQureshi <55016172+ShahzebQureshi@users.noreply.github.com>
2022-01-01 01:25:33 +0500

    Update and rename havoc_joan.mk to evolution_joan.mk

--- evolution_joan.mk
+++ evolution_joan.mk
@@ -1,5 +1,5 @@
 # Inherit some common AOSP stuff.
-$(call inherit-product, vendor/havoc/config/common.mk)
+$(call inherit-product, vendor/evolution/config/common.mk)
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
@@ -18,7 +18,7 @@ $(call inherit-product, device/lge/joan/device.mk)
 
 # Device identifiers
 PRODUCT_DEVICE := joan
-PRODUCT_NAME := havoc_joan
+PRODUCT_NAME := evolution_joan
 PRODUCT_BRAND := lge
 PRODUCT_MODEL := joan
 PRODUCT_MANUFACTURER := LGE
@@ -27,7 +27,7 @@ TARGET_BOOT_ANIMATION_RES := 1440
 TARGET_INCLUDE_GAPPS := false
 
 # praljenct randiant
-Cherish_BUILD_TYPE := UNOFFICIAL
+EVOLUTION_BUILD_TYPE := UNOFFICIAL
 
 PRODUCT_GMS_CLIENTID_BASE := android-om-lg
 
